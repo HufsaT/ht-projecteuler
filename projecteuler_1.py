b726db3014b2fe7aab3bf4cc26a36bb443d57bc9
HufsaT <htmustafa@gmail.com>
2017-09-09 11:39:55 -0400

    first commit

---
+++ projecteuler_1.py
@@ -0,0 +1,21 @@
+from tkinter import Tk, Label, Button
+
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
+
+        self.label = Label(master, text="This is our first GUI!")
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Greet", command=lambda: print("Greetings!"))
+        self.greet_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+
+
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
